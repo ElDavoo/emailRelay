5e60ff9391542a37dedf6b5df3994527e762a88f
Davide Palma (ElDavo) <github@davidepalma.it>
2024-09-17 12:54:06 +0200

    Add name support, fixes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 import json
 import requests
 import telethon
@@ -22,10 +21,9 @@ event_loop = asyncio.get_event_loop()
 event_loop.run_until_complete(client.connect())
 
 email_to_send = [email_from]
-@app.route('/webhooks/test', methods=['POST'])
+@app.route('/webhooks/master-thesis', methods=['POST'])
 @app.route('/', methods=['POST'])
 def test():
-    event_loop.run_until_complete(client.get_me())
     data = json.loads(request.data)
     print('Received:', request.data)
     reason = request.headers.get('X-GitHub-Event')
@@ -37,10 +35,10 @@ def test():
         if (data['action'] == 'completed'
                 and data['workflow_run']['conclusion'] == 'success'
                 and data['workflow_run']['status'] == 'completed'):
-            event_loop.run_until_complete(client.send_message('me', 'Workflow completed'))
+            #event_loop.run_until_complete(client.send_message('me', 'Workflow completed'))
             # Get the artifacts of the workflow
             artifacts_url = data['workflow_run']['artifacts_url']
-            event_loop.run_until_complete(client.send_message('me', artifacts_url))
+            #event_loop.run_until_complete(client.send_message('me', artifacts_url))
             # We should have only one artifact, so we get the first one
             response = requests.get(artifacts_url, headers={'Authorization': f'token {GITHUB_TOKEN}'})
             artifacts = response.json()['artifacts']
@@ -49,7 +47,7 @@ def test():
             artifact = response.json()['artifacts'][0]
             # Download the artifact
             download_url = artifact['archive_download_url']
-            event_loop.run_until_complete(client.send_message('me', download_url))
+            #event_loop.run_until_complete(client.send_message('me', download_url))
             artifact = requests.get(download_url, headers={'Authorization': f'token {GITHUB_TOKEN}'})
             # Unzip the artifact: There should me only one file, the thesis
             # we do not know the name of the file, so we need to extract it
@@ -57,21 +55,25 @@ def test():
                 file_name = z.namelist()[0]
                 thesis = z.read(file_name)
                 # Send the thesis to everyone in the list
-                for contact in telegram_contacts_to_send:
+                for contact, name in telegram_contacts_to_send.items():
                     event_loop.run_until_complete(
                         client.send_file(
-                            contact, thesis, caption='Thesis',
-                        attributes=[DocumentAttributeFilename('thesis.pdf')]))
+                            contact, thesis, caption=f'Ciao {name}, ti invio la nuova versione della tesi!',
+                            attributes=[DocumentAttributeFilename('thesis.pdf')]))
         return 'OK'
     if reason == 'release':
-        event_loop.run_until_complete(client.send_message('me', 'New release'))
         if data['action'] == 'published':
-            if len(data['release']['assets']) == 0:
-                return 'OK'
-            # from data['release']['assets'] find the first PDF (check content_type)
-            pdf = next(asset for asset in data['release']['assets'] if asset['content_type'] == 'application/pdf')
-            # download the PDF
-            pdf = requests.get(pdf['browser_download_url'], headers= {'Authorization': f'token {GITHUB_TOKEN}'})
+            # get the assets url
+            assets_url = data['release']['assets_url']
+            # get the assets
+            response = requests.get(assets_url, headers={'Authorization': f'token {GITHUB_TOKEN}'})
+            assets = response.json()
+            # get the pdf
+            pdf = next(asset for asset in assets if asset['content_type'] == 'application/pdf')
+            # download the pdf, set accept header to octet-stream
+            pdf = requests.get(pdf['url'], headers={'Accept': 'application/octet-stream',
+                                                                     'Authorization': f'token {GITHUB_TOKEN}'})
+            version = data['release']['tag_name']
             # Send email to everyone in the list and attach the PDF (gmail servers)
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
                 server.login(email_from, email_password)
@@ -79,8 +81,11 @@ def test():
                     msg = MIMEMultipart()
                     msg['From'] = email_from
                     msg['To'] = email
-                    msg['Subject'] = 'New release'
-                    msg.attach(MIMEText('New release'))
+                    msg['Subject'] = 'Tesi {}'.format(version)
+                    msg.attach(MIMEText('Gentile professore,\n'
+                                        'le invio la versione {} della mia tesi.\n'
+                                        'Attendendo un suo riscontro,\n'
+                                        'la saluto cordialmente.'.format(version)))
                     attachment = MIMEBase('application', 'octet-stream')
                     attachment.set_payload(pdf.content)
                     encoders.encode_base64(attachment)
@@ -88,7 +93,6 @@ def test():
                     msg.attach(attachment)
                     server.sendmail(email_from, email, msg.as_string())
         return 'OK'
-    event_loop.run_until_complete(client.send_message('me', 'Unknown event: ' + reason))
     return 'OK'
 
 
